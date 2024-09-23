53c1ed69514df6ad9309e5825581fe320db35fd1
IanBrown050922 <134359019+IanBrown050922@users.noreply.github.com>
2024-09-23 12:38:17 -0500

    Update demo.py

--- demo.py
+++ demo.py
@@ -3,8 +3,8 @@ import ripple_noise as rpn
 if __name__ == '__main__':
     # Configure the noise with a Ripple_Noise object.
     # Generate one or more images from a given configuration using the generate_image method.
-    noise1 = rpn.Ripple_Noise(size=500, density=12, scale=0.5, strength=1.5, smoothness=3)
-    noise1.generate_image('img_1')
+    noise_1 = rpn.Ripple_Noise(size=500, density=12, scale=0.5, strength=1.5, smoothness=3)
+    noise_1.generate_image('img_1')
 
     noise_2 = rpn.Ripple_Noise(size=300, density=3)
     noise_2.generate_image('img_2')
